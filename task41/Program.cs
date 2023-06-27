46d11645114af101dee410290d1975d27770b3b4
Olga <radenovic-family@yandex.ru>
2023-06-27 23:46:09 +0200

    Added tasks 41, 43

---
+++ task41/Program.cs
@@ -0,0 +1,55 @@
+﻿int inputNumber(string message) 
+{
+    Console.WriteLine(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void printArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        
+        if (i == array.Length - 1)
+        {
+            Console.Write(array[i] + ".");
+        }
+        else
+        {
+            Console.Write(array[i] + ", ");
+        }
+    }
+}
+
+
+int[] inputSomeNums(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Console.WriteLine("Введите число");
+        int number = Convert.ToInt32(Console.ReadLine());
+        array[i] = number;
+
+    }
+    return array;
+}
+
+int countNums(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int size = inputNumber("Сколько чисел вы хотите ввести?");
+int[] array = inputSomeNums(size);
+printArray(array);
+Console.WriteLine();
+Console.WriteLine($"Вы ввели {countNums(array)} чисел/ла больше 0");
---
+++ task43/Program.cs
@@ -0,0 +1,28 @@
+﻿double inputNumber(string message) 
+{
+    Console.WriteLine(message);
+    double number = Convert.ToDouble(Console.ReadLine());
+    return number;
+}
+
+double findX(double k1, double b1, double k2, double b2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    return x;
+}
+
+double findY(double k1, double b1, double x)
+{
+    double y = k1 * x + b1;
+    return y;
+}
+
+double k1 = inputNumber("Введите число k1");
+double b1 = inputNumber("Введите число b1");
+double k2 = inputNumber("Введите число k2");
+double b2 = inputNumber("Введите число b2");
+double x = findX(k1, b1, k2, b2);
+double y = findY(k1, b1, x);
+Console.WriteLine($"Точка пересечения: {x}, {y}");
+
+
